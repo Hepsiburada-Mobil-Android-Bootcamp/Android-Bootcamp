e4ba793d54e30b7287539e84984b5a4fef82ae61
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-09-27 11:53:41 +0300

    Test song class

--- app/src/main/java/com/android/odevler/abdulwahid/data/Song.kt
+++ app/src/main/java/com/android/odevler/abdulwahid/data/Song.kt
@@ -3,6 +3,14 @@ package com.android.odevler.abdulwahid.data
 data class Song(
     val name: String?,
     val artist: String?,
-    val duration: Int?,
-    val year: Int?
-)
+    val duration: Double,
+    val year: Int?,
+    var isPlaying: Boolean = false
+) {
+    fun playSong() {
+        isPlaying = true
+    }
+    fun stopSong() {
+        isPlaying = false
+    }
+}
