842b15f1b8f3e6b206f70b299be91c7309418e0c
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-09-27 12:45:47 +0300

    Design add new song layout

---
+++ app/src/main/java/com/android/odevler/abdulwahid/ui/AddNewSongActivity.kt
@@ -0,0 +1,12 @@
+package com.android.odevler.abdulwahid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.camp.R
+
+class AddNewSongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_song)
+    }
+}
---
+++ app/src/main/java/com/android/odevler/abdulwahid/ui/SongListActivity.kt
@@ -0,0 +1,12 @@
+package com.android.odevler.abdulwahid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.camp.R
+
+class SongListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
