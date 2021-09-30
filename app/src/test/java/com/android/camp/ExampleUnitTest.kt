56d7b901d45db6b0d16d1a3605ca567e8f9b1a63
serkan-ozdemir <56133743+serkan-ozdemir@users.noreply.github.com>
2021-09-30 22:24:48 +0300

    Odev-1&2

--- app/src/test/java/com/android/camp/ExampleUnitTest.kt
+++ app/src/test/java/com/android/camp/ExampleUnitTest.kt
@@ -16,16 +16,16 @@ class ExampleUnitTest {
     }
 
 
-    @Test
-    fun inteCevir_isCorrect() {
-        // başarılı senaryo kontrolü
-        assertTrue("deneme".inteCevir() == 0)
-        assertTrue("123".inteCevir() == 123)
-        assertTrue("10".inteCevir() == 10)
-
-        // başarısız senarya kontrolü
-        assertFalse("deneme".inteCevir() == 1)
-        assertFalse("123".inteCevir() == 0)
-        assertFalse("10".inteCevir() == 0)
-    }
+//    @Test
+//    fun inteCevir_isCorrect() {
+//        // başarılı senaryo kontrolü
+//        assertTrue("deneme".inteCevir() == 0)
+//        assertTrue("123".inteCevir() == 123)
+//        assertTrue("10".inteCevir() == 10)
+//
+//        // başarısız senarya kontrolü
+//        assertFalse("deneme".inteCevir() == 1)
+//        assertFalse("123".inteCevir() == 0)
+//        assertFalse("10".inteCevir() == 0)
+//    }
 }
