83b458f2a02c2ea9b3429d5e571c25fd102f3ff5
Ahmet Tarık Şahin <ahmetsahintarik@gmail.com>
2021-10-01 09:37:32 +0300

    1,2 Odevler

---
+++ app/src/main/java/com/android/odevler/ahmettariksahin/ComputerMainActivity.kt
@@ -0,0 +1,60 @@
+package com.android.odevler.ahmettariksahin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.camp.R
+import com.android.odevler.ahmettariksahin.computer.AddComputer
+import com.android.odevler.ahmettariksahin.computer.ComputerAdapter
+import com.android.odevler.ahmettariksahin.model.Computer
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ComputerMainActivity:AppCompatActivity() {
+    private val recyclerView by lazy { findViewById<RecyclerView>(R.id.computers) }
+    private val addFlightButton by lazy { findViewById<Button>(R.id.addComputer) }
+
+    var firestore: FirebaseFirestore? = null
+
+    val computers = arrayListOf<Computer>(
+        Computer("500w Gold+","AOC","ASUS","16GB Corsair", "RAZER"),
+        Computer("600w Gold+","ASUS","MSI","32GB Corsair", "RAMPAGE"),
+        Computer("750w Gold+","HP","MSI","8GB Corsair", "STEELSERIES"),
+        Computer("450w Gold+","DELL","ASUS","32GB Corsair", "RAZER"),
+    )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_computer_main)
+        addFlightButton.setOnClickListener {
+            val intent = Intent(this,AddComputer::class.java)
+            startActivity(intent)
+        }
+
+        firestore = FirebaseFirestore.getInstance()
+        initComputeers()
+        bindComputeers()
+
+    }
+
+    private fun initComputeers(){
+        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+    }
+    private fun bindComputeers(){
+        firestore?.collection("ahmettariksahin")?.addSnapshotListener { value, _ ->
+            val list = arrayListOf<Computer>()
+
+            value?.forEach { queryDocumentSnapshot ->
+                queryDocumentSnapshot.toObject(Computer::class.java).also { flight ->
+                    list.add(flight)
+                }
+            }
+
+            recyclerView.adapter = ComputerAdapter(list)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/android/odevler/ahmettariksahin/computer/AddComputer.kt
@@ -0,0 +1,73 @@
+package com.android.odevler.ahmettariksahin.computer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.android.camp.R
+import com.android.odevler.ahmettariksahin.model.Computer
+import com.google.firebase.firestore.FirebaseFirestore
+
+class AddComputer : AppCompatActivity() {
+    private val powerSupply: EditText by lazy { findViewById(R.id.power) }
+    private val screenModel: EditText by lazy { findViewById(R.id.screen) }
+    private val motherBoard: EditText by lazy { findViewById(R.id.motherBoard) }
+    private val ram: EditText by lazy { findViewById(R.id.ram) }
+    private val keyboard: EditText by lazy { findViewById(R.id.keyboard) }
+    private val addButton by lazy { findViewById<Button>(R.id.add) }
+
+    private var firestore: FirebaseFirestore? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_computer)
+        addButton.setOnClickListener{
+            if (isValid()) {
+                save()
+            }
+        }
+        firestore = FirebaseFirestore.getInstance()
+
+    }
+
+    private fun save() {
+        val flight = Computer(powerSupply = powerSupply.text.toString(),
+            screenModel = screenModel.text.toString(),
+            motherBoard = motherBoard.text.toString(),
+            ram = ram.text.toString(),
+            keyboard = keyboard.text.toString())
+        firestore?.collection("ahmettariksahin")?.add(flight)?.addOnCompleteListener { task ->
+            when (task.isSuccessful) {
+                true -> finish()
+                false -> Toast.makeText(this, "Pc not added:(", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    fun EditText.isValid(): Boolean {
+        if (text.isNullOrEmpty()) {
+            requestFocus()
+            error = "bu alan gereklii.."
+        }
+
+        return text.isNotEmpty()
+    }
+
+    private fun isValid(): Boolean {
+        var isValid = true
+
+        arrayListOf(
+            powerSupply,
+            screenModel,
+            motherBoard,
+            ram,
+            keyboard
+        ).forEach { editText ->
+            isValid = editText.isValid() && isValid
+        }
+
+        return isValid
+    }
+}
