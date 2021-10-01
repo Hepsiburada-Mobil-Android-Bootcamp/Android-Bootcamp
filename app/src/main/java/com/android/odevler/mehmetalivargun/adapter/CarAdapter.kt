b31fe16188773e048d4c7dd1cefc54b4ffdadb7f
mehmetalivargun <34211151+mehmetalivargun@users.noreply.github.com>
2021-10-01 12:22:15 +0300

    Odev2 Car Class and Activities

---
+++ app/src/main/java/com/android/odevler/mehmetalivargun/adapter/CarAdapter.kt
@@ -0,0 +1,27 @@
+package com.android.odevler.mehmetalivargun.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.camp.R
+import com.android.odevler.mehmetalivargun.data.Car
+
+class CarAdapter(private val context:Context,private val cars:ArrayList<Car>):RecyclerView.Adapter<CarViewHolder>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
+      return CarViewHolder(
+          LayoutInflater.from(parent.context).inflate(R.layout.item_car,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+       val car= cars[position]
+        holder.bind(car)
+
+    }
+
+    override fun getItemCount()=cars.size
+
+}
