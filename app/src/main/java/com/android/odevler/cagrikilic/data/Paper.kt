f74077cfb6b96dd9282a1abd0a9d272805559e40
root999 <cagriiikilic@gmail.com>
2021-10-02 03:00:42 +0300

    ödev2 ikinci commit

--- app/src/main/java/com/android/odevler/cagrikilic/data/Paper.kt
+++ app/src/main/java/com/android/odevler/cagrikilic/data/Paper.kt
@@ -1,12 +1,12 @@
 package com.android.odevler.cagrikilic.data
 
 data class Paper(
-    val name:String,
-    val color: String,
-    val width: Int,
-    val height : Int,
+    val name:String ?="",
+    val color: String?="",
+    val width: Int ?=null,
+    val height : Int ?=null,
 ){
     fun calculateArea(): Int {
-        return width * height
+        return width!! * height!!
     }
 }
