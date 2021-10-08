bf8d329370b874eeade771f1d0dc1c5b4b170be7
mehmetalivargun <34211151+mehmetalivargun@users.noreply.github.com>
2021-10-08 12:09:07 +0300

    Added Polish Language and Car List and Car Add  converted to Data Binding form

---
+++ app/src/main/java/com/android/firebase/auth/yahoo/YahooLogin.kt
@@ -0,0 +1,41 @@
+package com.android.firebase.auth.yahoo
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.android.camp.R
+import com.android.camp.databinding.ActivityYahooLoginBinding
+import com.android.camp.login.LoginFragment
+import com.google.firebase.auth.FirebaseAuth
+
+
+class YahooLogin : AppCompatActivity() {
+
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    private lateinit var binding: ActivityYahooLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityYahooLoginBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val loginFragment = YahooAuthFragment()
+        openFragment(loginFragment)
+
+
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val manager = supportFragmentManager.beginTransaction()
+        manager.replace(R.id.yahoo_frame, fragment)
+        manager.commitAllowingStateLoss()
+    }
+
+
+}
