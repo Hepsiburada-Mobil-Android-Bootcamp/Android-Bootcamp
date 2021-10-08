979df83afabef343d7499032b2924b409ed24d03
Ahmet Tarık Şahin <ahmetsahintarik@gmail.com>
2021-10-08 23:13:42 +0300

    odev 3

--- app/src/main/java/com/android/odevler/nahitfidanci/GalaxyHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.android.odevler.nahitfidanci
-
-import com.android.odevler.nahitfidanci.data.Galaxy
-
-object GalaxyHelper {
-    var galaxyList = arrayListOf<Galaxy>(
-        Galaxy("Milkway", 121564613, 6565546134, 1885)
-    )
-}
--- app/src/main/java/com/android/odevler/orhunozdemir/data/Customer.kt
+++
@@ -1,7 +0,0 @@
-package com.android.odevler.orhunozdemir.data
-
- data class Customer (var name:String?="",var surname:String?="",var age:Int?=0)
- {
-     fun fullname():String= "$name-$surname"
-
-}
