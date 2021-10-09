fa9f7be984f4db1f92ecd5ecc91d64e9e6846096
edakuntalp <eda.kuntalp@ceng.deu.edu.tr>
2021-10-09 20:37:06 +0300

    eda commit

---
+++ app/src/main/java/com/android/camp/firebase/auth/twitter/TwitterLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.android.camp.firebase.auth.twitter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.camp.R
+
+class TwitterLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter_login)
+    }
+}
