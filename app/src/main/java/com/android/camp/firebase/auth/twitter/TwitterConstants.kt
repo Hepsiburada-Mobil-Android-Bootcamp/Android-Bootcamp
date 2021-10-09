d95e04f8c99bdd628bff0aee361d59a48640b021
edakuntalp <eda.kuntalp@ceng.deu.edu.tr>
2021-10-09 22:01:12 +0300

    eda commit 2

---
+++ app/src/main/java/com/android/camp/firebase/auth/twitter/TwitterConstants.kt
@@ -0,0 +1,7 @@
+package com.android.camp.firebase.auth.twitter
+
+object TwitterConstants {
+    var CONSUMER_KEY = "bu6zAz2sMS47rHmTy4m34Dw71";
+    var CONSUMER_SECRET = "5pagSBnKGT8mKDITwPvUad4svRu8HyTnIz4djA5lSbu0jDkvFv";
+    var CALLBACK_URL = "twitterloginbootcampapp://";
+}
--- app/src/main/java/com/android/camp/firebase/auth/twitter/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.android.camp.firebase.auth.twitter.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
