5cdde4313f8f05fdd1317dabed63289471ff1329
edakuntalp <eda.kuntalp@ceng.deu.edu.tr>
2021-10-09 23:12:43 +0300

    twitter login

--- app/src/main/java/com/android/camp/firebase/auth/twitter/TwitterConstants.kt
+++ app/src/main/java/com/android/camp/firebase/auth/twitter/TwitterConstants.kt
@@ -3,5 +3,5 @@ package com.android.camp.firebase.auth.twitter
 object TwitterConstants {
     var CONSUMER_KEY = "bu6zAz2sMS47rHmTy4m34Dw71";
     var CONSUMER_SECRET = "5pagSBnKGT8mKDITwPvUad4svRu8HyTnIz4djA5lSbu0jDkvFv";
-    var CALLBACK_URL = "twitterloginbootcampapp://";
+    var CALLBACK_URL = "https://camp-c7843.firebaseapp.com/__/auth/handler";
 }
